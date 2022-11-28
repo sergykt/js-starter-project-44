0d75556a9b61dd6eaddfa5a4e0e0bb890fc26541
Sergei Sivtsev <sergykt1992@mail.ru>
2022-11-28 12:59:07 +0600

    step 7

---
+++ src/games/gcd.js
@@ -0,0 +1,36 @@
+import {
+  askName, randomInteger, isAnswerCorrect, getAnswer,
+} from '../index.js';
+
+const gcd = (userName, k = 0) => {
+  if (k === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
+  let x = randomInteger();
+  let y = randomInteger();
+  let z;
+  console.log(`Question: ${x} ${y}`);
+  if (x < y) {
+    z = x;
+    x = y;
+    y = z;
+  }
+  while (x % y !== 0) {
+    z = x % y;
+    x = y;
+    y = z;
+  }
+  const correctAnswer = y;
+  const answer = Number(getAnswer());
+  const result = isAnswerCorrect(answer, correctAnswer, userName);
+  if (result) gcd(userName, k + 1);
+};
+
+const game = () => {
+  const name = askName();
+  console.log('Find the greatest common divisor of given numbers.');
+  gcd(name);
+};
+
+export default game;
